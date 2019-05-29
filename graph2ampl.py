4dc9b475bb9f899eb77176ede2c2e0aad9ae80fe
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2019-05-29 15:33:38 +0200

    Add dummy AP coverage probability generation

--- graph2ampl.py
+++ graph2ampl.py
@@ -5,15 +5,16 @@ from amplpy import AMPL, DataFrame
 import argparse
 
 InfraTypes = {
-    'AP': "AP",
-    'server': "server",
-    'mobile': "mobile"
+    'APs': "AP",
+    'servers': "server",
+    'mobiles': "mobile"
 }
 
+infra_type_sets = {itype: [] for itype in InfraTypes.values()}
 
 def fill_service(ampl: AMPL, service: nx.classes.graph.Graph) -> None:
     vnfs, demands = [], []
-    service_graph = ampl.param['serviceGraph']
+    service_graph = ampl.param['serviceGraph'].value()
 
     for vnf_id, vnf_data in service.nodes(data=True):
         vnfs += [vnf_data['name']]
@@ -32,21 +33,20 @@ def fill_service(ampl: AMPL, service: nx.classes.graph.Graph) -> None:
 
 
 def fill_infra(ampl: AMPL, infra: nx.classes.graph.Graph) -> None:
-    infra_graph = ampl.param['infraGraph']
+    infra_graph = ampl.param['infraGraph'].value()
     infra_set = []
-    infra_type_sets = {itype: [] for itype in InfraTypes.keys()}
     resources = {}
     cost_unit_demand = {}
     for infra_id, infra_data in infra.nodes(data=True):
         infra_name = infra_data['name']
         infra_set.append(infra_name)
-        infra_type_sets[infra_data['type']].append(infra_id)
+        infra_type_sets[infra_data['type']].append(infra_name)
         resources[infra_name] = infra_data['resource']
         cost_unit_demand[infra_name] = infra_data['cost']
 
     # Set AMPL object
-    for itype in InfraTypes.keys():
-        ampl.set[itype] = infra_type_sets[itype]
+    for itype, infra_set_name in InfraTypes.items():
+        ampl.set[itype] = infra_type_sets[infra_set_name]
 
     ampl.set['vertices'][infra_graph] = infra_set
     df = DataFrame('vertices', 'resources')
@@ -59,9 +59,11 @@ def fill_infra(ampl: AMPL, infra: nx.classes.graph.Graph) -> None:
 
 
 def fill_AP_coverage_probabilities(ampl: AMPL, interval_length: int) -> None:
-    for i in range(1, interval_length + 1):
-        for AP_name in ampl.set[InfraTypes['AP']]:
-            ampl.param['prob_AP'][i][AP_name] = 0.9
+    subintervals = [i for i in range(1, interval_length + 1)]
+    APs = infra_type_sets[InfraTypes['APs']]
+    df = DataFrame(('AP_name', 'subinterval'), 'prob')
+    df.setValues({(AP, subint): 0.9 for AP in APs for subint in subintervals})
+    ampl.param['prob_AP'].setValues(df)
 
 
 if __name__ == '__main__':
@@ -89,8 +91,10 @@ if __name__ == '__main__':
     ampl = AMPL()
     ampl.read(args.model)
     ampl.set['graph'] = [args.infra, args.service]
-    ampl.param['infraGraph'] = [args.infra]
-    ampl.param['serviceGraph'] = [args.service]
+    ampl.param['infraGraph'] = args.infra
+    ampl.param['serviceGraph'] = args.service
+    ampl.param['interval_length'] = args.interval_length
+    ampl.param['coverage_threshold'] = 0.95
 
     fill_service(ampl, service)
     fill_infra(ampl, infra)
