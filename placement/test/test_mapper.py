8a417bdeb96e3434916446afcc3de7c829ddcdc6
Jorge Martín Pérez <jmartinp@it.uc3m.es>
2019-06-04 18:08:58 +0200

    Write test scenario

--- placement/test/test_mapper.py
+++ placement/test/test_mapper.py
@@ -186,31 +186,6 @@ class TestGreedyFogCostMapper(unittest.TestCase):
         self.assertEqual(self.mapping_k1['control'], 'h1')
         self.assertEqual(self.mapping_k1['robot', 'AP'], ['f1', 'a1'])
         self.assertEqual(self.mapping_k1['AP', 'control'], ['a1', 'sw1', 'h1'])
-        # Check mapping delay for depth k=1
-        expected_delay = self.infra['f1']['a1']['delay'] +\
-                         self.infra['a1']['sw1']['delay'] +\
-                         self.infra['sw1']['h1']['delay']
-        mk1 = dict(self.mapping_k1)
-        self.assertEqual(mapper.GreedyFogCostMapper.map_delay(
-                             infra=self.infra, ns=self.ns,
-                             mapping=self.mapping_k1), expected_delay)
-        # Check mapping cost depth k=1
-        expected_cost = self.infra['f1']['a1']['cost'] *\
-                            self.ns['robot']['AP']['bw'] +\
-                        (self.infra['a1']['sw1']['cost'] +\
-                         self.infra['a1']['sw1']['cost']) *\
-                            self.ns['AP']['control']['bw']
-        for v,h in [('robot', 'f1'), ('AP', 'a1'), ('control', 'h1')]:
-            for r in ['cpu', 'mem', 'disk']:
-                expected_cost += self.infra.nodes[h]['cost'][r] *\
-                                 self.ns.nodes[v][r]
-        self.assertEqual(mapper.GreedyFogCostMapper.map_cost(
-                             infra=self.infra, ns=self.ns,
-                             mapping=self.mapping_k1), expected_cost)
-        # Check the lifetime with depth k=1
-        self.assertEqual(mapper.GreedyFogCostMapper.map_lifetime(
-                             infra=self.infra, mapping=self.mapping_k1), 80)
-
 
         # Check correct mapping for depth k=2
         self.assertTrue(self.mapping_k2['worked'])
@@ -219,33 +194,157 @@ class TestGreedyFogCostMapper(unittest.TestCase):
         self.assertEqual(self.mapping_k2['control'], 'h1')
         self.assertEqual(self.mapping_k2['robot', 'AP'], ['f1', 'a1'])
         self.assertEqual(self.mapping_k2['AP', 'control'], ['a1', 'sw2', 'h1'])
-        self.assertEqual(mapper.GreedyFogCostMapper.map_delay(
-                             infra=self.infra, ns=self.ns,
-                             mapping=self.mapping_k2), 3)
-        # Check mapping delay for depth k=2
-        expected_delay = self.infra['f1']['a1']['delay'] +\
-                         self.infra['a1']['sw2']['delay'] +\
-                         self.infra['sw2']['h1']['delay']
-        mk1 = dict(self.mapping_k1)
-        self.assertEqual(mapper.GreedyFogCostMapper.map_delay(
-                             infra=self.infra, ns=self.ns,
-                             mapping=self.mapping_k2), expected_delay)
-        # Check mapping cost depth k=2
-        expected_cost = self.infra['f1']['a1']['cost'] *\
-                            self.ns['robot']['AP']['bw'] +\
-                        (self.infra['a1']['sw2']['cost'] +\
-                         self.infra['a1']['sw2']['cost']) *\
-                            self.ns['AP']['control']['bw']
-        for v,h in [('robot', 'f1'), ('AP', 'a1'), ('control', 'h1')]:
-            for r in ['cpu', 'mem', 'disk']:
-                expected_cost += self.infra.nodes[h]['cost'][r] *\
-                                 self.ns.nodes[v][r]
-        self.assertEqual(mapper.GreedyFogCostMapper.map_cost(
-                             infra=self.infra, ns=self.ns,
-                             mapping=self.mapping_k2), expected_cost)
-        # Check the lifetime with depth k=2
+
+
+    def test_reliability(self):
+        """Checks if the mapping reliability retrieval works
+        :returns: None
+
+        """
+        self.assertEqual(mapper.GreedyFogCostMapper.map_reliability(
+                         infra=self.infra, mapping=self.mapping_k1), 1/4)
+        self.assertEqual(mapper.GreedyFogCostMapper.map_reliability(
+                         infra=self.infra, mapping=self.mapping_k2), 1/4)
+
+
+    def test_lifetime(self):
+        """Checks if lifetime retrieval works
+        :returns: None
+
+        """
         self.assertEqual(mapper.GreedyFogCostMapper.map_lifetime(
-                             infra=self.infra, mapping=self.mapping_k2), 80)
+                         infra=self.infra, mapping=self.mapping_k1), 80)  
+        self.assertEqual(mapper.GreedyFogCostMapper.map_lifetime(
+                         infra=self.infra, mapping=self.mapping_k2), 80)  
+        
+
+class TestFPTASMapper(unittest.TestCase):
+
+    """Test case for the FPTASMapper class"""
+
+
+    def __graphs_setup(self) -> None:
+        """Create the infrastructure and network service graphs
+        :returns: None
+
+        """
+        # Create the infrastructure graph
+        infra = nx.DiGraph()
+        a1_cost = {'cpu': 2, 'disk': 2.3, 'mem': 4}
+        a2_cost = {'cpu': 4, 'disk': 4.6, 'mem': 8}
+        a3_cost = {'cpu': 4, 'disk': 4.6, 'mem': 8}
+        f1_cost = {'cpu': 4, 'disk': 6, 'mem': 8}
+        f2_cost = {'cpu': 4, 'disk': 6, 'mem': 8}
+
+        infra.add_node('f1', cpu=1, mem=8, disk=512, cost=f_cost,
+                       reliability=1, lifetime=80,
+                       location=(39.128380, -1.080805))
+        infra.add_node('f2', cpu=1, mem=8, disk=512, cost=f_cost,
+                       reliability=0.8, lifetime=80,
+                       location=(39.132474, -1.087976))
+        infra.add_node('a1', cpu=1, mem=16, disk=1024,
+                       rats=['LTE', 'MMW'], location=(39.1408046,-1.0795603),
+                       cost=a_cost, reliability=1, lifetime=100)
+        infra.add_node('a2', cpu=1, mem=16, disk=1024,
+                       rats=['LTE', 'MMW'], location=(39.1408046,-1.0795603),
+                       cost=a_cost, reliability=1, lifetime=100)
+        infra.add_node('a3', cpu=1, mem=16, disk=1024,
+                       rats=['LTE', 'MMW'], location=(39.1408046,-1.0795603),
+                       cost=a_cost, reliability=1, lifetime=100)
+        infra.add_node('sw1', cpu=0, mem=0, disk=0, cost=sw_cost,
+                       reliability=1, lifetime=100)
+        infra.add_node('sw2', cpu=0, mem=0, disk=0, cost=sw_cost,
+                       reliability=1, lifetime=100)
+        infra.add_node('h1', cpu=0, mem=0, disk=0, cost=h_cost,
+                       reliability=1, lifetime=100)
+        infra.add_node('h2', cpu=0, mem=0, disk=0, cost=h_cost,
+                       reliability=1, lifetime=100)
+
+        # Fog-antennas' links
+        infra.add_edge('f1', 'a1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('f2', 'a1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('f1', 'a2', bw=100, delay=1, cost=10, reliability=0.8)
+        infra.add_edge('f2', 'a2', bw=100, delay=1, cost=10, reliability=0.8)
+        infra.add_edge('f1', 'a3', bw=100, delay=1, cost=5, reliability=0.7)
+        infra.add_edge('f2', 'a3', bw=100, delay=1, cost=5, reliability=0.7)
+
+        # Antenna-switches' links
+        infra.add_edge('a1', 'sw1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('a1', 'sw2', bw=100, delay=1, cost=20, reliability=0.9)
+        infra.add_edge('a2', 'sw1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('a2', 'sw2', bw=100, delay=1, cost=20, reliability=0.9)
+        infra.add_edge('a3', 'sw1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('a3', 'sw2', bw=100, delay=1, cost=20, reliability=0.9)
+
+        # Switches and servers
+        infra.add_edge('sw1', 'h1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('sw2', 'h2', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('sw1', 'h1', bw=100, delay=1, cost=20, reliability=1)
+        infra.add_edge('sw2', 'h2', bw=100, delay=1, cost=20, reliability=1)
+
+        self.infra = infra
+        
+        # Create the network service graph
+        ns = nx.DiGraph()
+        ns.add_node('robot_master', cpu=1, mem=1, disk=400, lv=1,
+                    location={'center': (39.128380, -1.080805), 'radius': 1})
+        ns.add_node('AP', cpu=1, mem=4, disk=100, rats=['LTE', 'MMW'], lv=1
+            location={'center': (39.1408046,-1.0795603), 'radius': 2})
+        ns.add_node('robot_slave', cpu=1, mem=1, disk=400, lv=1,
+                    location={'center': (39.132474, -1.087976), 'radius': 1})
+        ns.add_edge('robot_master', 'AP', bw=1, delay=100)
+        ns.add_edge('AP', 'robot_slave', bw=1, delay=100)
+        self.ns = ns
+
+
+    def __mapper_set_up(self) -> None:
+        """Creates the greedy fog mappers for the tests
+        :returns: None
+
+        """
+        self.checker = checker.CheckFogDigraphs()
+        self.mapper = mapper.FPTASMapper(checker=self.checker,
+                                         log_out='/tmp/fptas_test.log')
+
+    def __map_suite(self) -> None:
+        """Performs the mapping of the network service(s) in the test
+        :returns: None
+
+        """
+        # e2e-delay=400, reliability=1
+        self.infra['robot_master']['delay'] = 400
+        self.infra['robot_master']['reliability'] = 1
+        self.mapping_d400_r1_k1_t1_rel1 = self.mapper_k1.map(infra=self.infra,
+                                                             ns=self.ns, k=1,
+                                                             tau=1, relax=1)
+
+
+    def setUp(self):
+        self.__graphs_setup()
+        self.__mapper_set_up()
+        self.__map_suite()
+
+
+    def test_map(self):
+        """Checks if the mappings work
+        :returns: None
+
+        """
+        # Check correct mapping for depth k=1
+        self.assertTrue(self.mapping_k1['worked'])
+        self.assertEqual(self.mapping_k1['robot'], 'f1')
+        self.assertEqual(self.mapping_k1['AP'], 'a1')
+        self.assertEqual(self.mapping_k1['control'], 'h1')
+        self.assertEqual(self.mapping_k1['robot', 'AP'], ['f1', 'a1'])
+        self.assertEqual(self.mapping_k1['AP', 'control'], ['a1', 'sw1', 'h1'])
+
+        # Check correct mapping for depth k=2
+        self.assertTrue(self.mapping_k2['worked'])
+        self.assertEqual(self.mapping_k2['robot'], 'f1')
+        self.assertEqual(self.mapping_k2['AP'], 'a1')
+        self.assertEqual(self.mapping_k2['control'], 'h1')
+        self.assertEqual(self.mapping_k2['robot', 'AP'], ['f1', 'a1'])
+        self.assertEqual(self.mapping_k2['AP', 'control'], ['a1', 'sw2', 'h1'])
 
 
     def test_reliability(self):
